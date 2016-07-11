576e9617cf23be24fb58c6a41c911584e4e6dedc
stefy83 <stefy83@gmail.com>
2016-07-11 12:19:11 +0200

    fff

--- App_MVC_COMPLETE/App_MVC_COMPLETE/ICOLibraryAPI.swift
+++ App_MVC_COMPLETE/App_MVC_COMPLETE/ICOLibraryAPI.swift
@@ -11,11 +11,17 @@ import UIKit
 class ICOLibraryAPI: NSObject {
     
     //Creamos el patron Singleton
+    //1.Creamos una variable de clase como una variable de tipo computarizada (OBJC)
     
     class var sharedInstance : ICOLibraryAPI{
+        //2. Anidamos dentro de la variable una struct llamada Singleton
+        //c++ y de Haskell -> Swift
         struct Singleton {
+            //3. Envuelve una constante estatica, esta es la variable implicitamente perezosa, es decir,
+            //se crea cuando sea absolutamente necesario, y una unica vez.
             static let instance = ICOLibraryAPI()
         }
+        //4. Devuelve la propiedad de tipo computerizado
         return Singleton.instance
     }
 
