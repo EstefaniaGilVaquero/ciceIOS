475cb7cc90780bcc63ee08e2f69b3a0f4095d682
stefy83 <stefy83@gmail.com>
2016-07-12 19:16:32 +0200

    no funciona el indicator activity

--- App_MVC_Repaso/App_MVC_Repaso/ViewController.swift
+++ App_MVC_Repaso/App_MVC_Repaso/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
                                     pEmail: "stefy83@gmail.com",
                                     pLoremIpsum: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.",
                                     pFotoPerfil: UIImage(named:"manolo.jpg")!,
-                                    pUrlWebSite: NSURL(string: "http://as.com/")!)
+                                  //  pUrlWebSite: NSURL(string: "http://as.com/")!)
+                                    pUrlWebSite: NSURL(string: "https://trakt.tv/")!)
+    
     
     //MARK: - IBOUTLET
     
