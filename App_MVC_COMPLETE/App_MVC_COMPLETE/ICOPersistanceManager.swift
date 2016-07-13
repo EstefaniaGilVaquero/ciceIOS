815834868031eeac74201850a1ea1b014d0c9a80
EstefaniaGilVaquero <stefy83@gmail.com>
2016-07-13 09:10:13 +0200

    Añado la clase persistanceManager

---
+++ App_MVC_COMPLETE/App_MVC_COMPLETE/ICOPersistanceManager.swift
@@ -0,0 +1,13 @@
+//
+//  ICOPersistanceManager.swift
+//  App_MVC_COMPLETE
+//
+//  Created by cice on 13/7/16.
+//  Copyright © 2016 cice. All rights reserved.
+//
+
+import UIKit
+
+class ICOPersistanceManager: NSObject {
+
+}
