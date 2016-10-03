5b2e748dc226623fa165444d1865de62e88a9e38
stefy83@gmail.com <stefy83@gmail.com>
2016-10-03 12:21:23 +0200

    borro

--- App_VolksWagenFinal-/App_volksWagenStefy/Pods/Target Support Files/Pods-App_VolksWagenFinal_CICE/Pods-App_VolksWagenFinal_CICE-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_App_VolksWagenFinal_CICEVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_App_VolksWagenFinal_CICEVersionString[];
-
